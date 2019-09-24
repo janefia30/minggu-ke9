e9adfc703e4e6bbd6f0dc116a38e2396453b47be
Jane Nur Fajriah 51 <janefria30@gmail.com>
2019-09-24 09:31:24 +0700

    blabla

--- latihanexpress/latihanexpress.js
+++ latihanexpress/latihanexpress.js
@@ -4,6 +4,20 @@ const port = 8000;
 
 app.get('/',(reg,res)=>{
     res.send('Halloooo aku express');
-})
+});
+
+app.get('/login',(reg,res)=>{
+    res.send('LOGIN!!');
+});
+app.post('/resgister',(reg,res)=>{
+    res.send('Request POST for register')
+});
+app.put('/puts',(reg,res)=>{
+    res.send('Request Put for put')
+});
+app.delete('/hapus',(reg,res)=>{
+    res.send('Delete  for is')
+});
+
 
 app.listen(port,()=> console.log(`berhasil di jalankan $(port)`))
